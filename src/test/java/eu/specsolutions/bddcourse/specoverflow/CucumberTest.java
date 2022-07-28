eee939cdda564b29244a07a1b980d4da44326640
Gaspar Nagy <gaspar.nagy@gmail.com>
2022-07-28 11:32:22 +0200

    rename test class

--- src/test/java/eu/specsolutions/bddcourse/specoverflow/CucumberTest.java
+++ src/test/java/eu/specsolutions/bddcourse/specoverflow/CucumberTest.java
@@ -11,5 +11,5 @@ import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("eu/specsolutions/bddcourse/specoverflow")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "eu.specsolutions.bddcourse.specoverflow")
-public class CucumberControllerTest {
+public class CucumberTest {
 }
